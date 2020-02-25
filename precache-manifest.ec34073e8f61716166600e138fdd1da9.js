35288d87984fd1a579d41d8a28d8c579ba850dff
Rafael Ricardo <rafael_rikardo@yahoo.com.br>
2020-02-25 16:20:05 +0000

    Updates

---
+++ precache-manifest.ec34073e8f61716166600e138fdd1da9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0715a312210aa858ce6d89ad6dd8fc0",
+    "url": "/mymeals/index.html"
+  },
+  {
+    "revision": "c215df1c1b66a7f408de",
+    "url": "/mymeals/static/css/main.48b541e5.chunk.css"
+  },
+  {
+    "revision": "04a239af550066c240bc",
+    "url": "/mymeals/static/js/2.b904dd10.chunk.js"
+  },
+  {
+    "revision": "b46e53c35c7c467d4914868fe3cf387d",
+    "url": "/mymeals/static/js/2.b904dd10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c215df1c1b66a7f408de",
+    "url": "/mymeals/static/js/main.611c6a8e.chunk.js"
+  },
+  {
+    "revision": "d51f1f7ddca7fb759ed3",
+    "url": "/mymeals/static/js/runtime-main.39d20bb7.js"
+  }
+]);
--- src/utils/helper.js
+++
@@ -1,5 +0,0 @@
-export function capitalize(str = ''){
-    return typeof str !== 'string'
-        ? ''
-        : str[0].toUpperCase().concat(str.slice(1))
-}
